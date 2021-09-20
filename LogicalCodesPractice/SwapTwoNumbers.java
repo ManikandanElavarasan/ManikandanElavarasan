8f4e83edc7d2accc19602ded782624b8352704c1
MANIKANDAN E <89540495+ManikandanElavarasan@users.noreply.github.com>
2021-09-20 14:06:46 +0530

    Update SwapTwoNumbers.java

--- LogicalCodesPractice/SwapTwoNumbers.java
+++ LogicalCodesPractice/SwapTwoNumbers.java
@@ -3,30 +3,36 @@ package swap;
 public class num {
 
 	public static void main(String[] args) {
-		int a=3,b=2,temp;
+		
+		//first way using third variable.
+		  int a=3,b=2,temp;
 		  System.out.println("before swapping a and b values are"+a +b);   
 		  temp=a;                                                      
-	      a=b;
+	          a=b;
 		  b=temp;
 		  System.out.println("After swapping a and b values are"+a +b);
 		  
-		  //first way using another variable.
 		  
-		 
+		  
+		 //Second way 
 		  System.out.println("before swapping a and b values are"+a +b);  
 		  a=a^b; 
 		  b=a^b;
 		  a=a^b;
 		  System.out.println("after swapping a and b values are"+a +b);
-		  //second way
 		
+		
+		  
+		//third way
 		System.out.println("before swapping a and b values are"+a +b);  
 		a=a+b; //a=5      
 		b=a-b; //b=3
 		a=a-b; //a=2
 		System.out.println(" after swapping the a  and b values are"+a +b);
 		
-		//third way
+		
+		
+		//fourth way
 		System.out.println("before swapping a and b values are"+a +b);  
 		b=a+b-(a=b);
 		System.out.println(" after swapping the a  and b values are"+a +b);
