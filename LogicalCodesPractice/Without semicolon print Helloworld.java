7ebe647d5e6722166f6d004831572e60e9924a49
MANIKANDAN E <89540495+ManikandanElavarasan@users.noreply.github.com>
2021-09-20 14:08:39 +0530

    Add files via upload

---
+++ LogicalCodesPractice/Without semicolon print Helloworld.java
@@ -0,0 +1,21 @@
+ package withoutsemi;
+
+public class wt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        if((System.out.printf("helloworld"))==null)   //1 way
+        {
+        	
+        }
+        if((System.out.append("Hello World"))==null)  //2 way
+        {
+        	
+        }
+        if((System.out.printf("Hello World").equals(null)))  //3 way
+        {
+        	
+        }
+	}
+
+}
