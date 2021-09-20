36526138e409fbc7703a86d0a7d0d0fc2d8234c6
MANIKANDAN E <89540495+ManikandanElavarasan@users.noreply.github.com>
2021-09-20 13:38:23 +0530

    Update FindDupliacteElemenet.java

--- LogicalCodesPractice/FindDupliacteElemenet.java
+++ LogicalCodesPractice/FindDupliacteElemenet.java
@@ -9,38 +9,13 @@ public class count {
 
 	public static void main(String[] args) {
 		
-		//C.find("Javascript",'v');
-		//C.find1("java", 'v');
-		C.dupliacte("Maha");
+		
+		C.dupliacte("JAVA");
 }
 
 //find occurence characters in a string
 static class C
 {
-	/*static int count1=0;
-	public static void find(String s,char c)
-	{
-		for(char ch:s.toCharArray()) {
-			if(ch==c)
-			count1++;
-			
-		}
-		System.out.println(c+":"+count1);
-	}
-	public static void find1(String s1,char val)
-	{
-		int count=0;
-	
-		for(int i=0;i<s1.length();i++)
-		{
-			if(val==s1.charAt(i))
-			{
-				count++;
-			}
-		}
-		System.out.println(val+":"+count);
-		
-	} */
 	
 	//find duplicate element and we can print occurence of the eleements....
 	public static void dupliacte(String str)
@@ -82,15 +57,19 @@ static class C
 			{
 				System.out.println(entry.getKey()+" : "+entry.getValue());
 			}
-		}
-		
+	        }  
+		/*
+	       if we want to print Occurence of letters we can print that as well using HashMap
+	       
+	       
+		for( Map.Entry<Character,Integer> entry :entryset)
+		{
+			if(entry.getValue()>=1)
+			{
+				System.out.println(entry.getKey()+" : "+entry.getValue());
+			}
+	        }*/
 	
-		
-		
-		
-		
-		
-	}
 	
 	
 	
