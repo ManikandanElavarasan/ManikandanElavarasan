4a2535ac6be26a927d91f4e9bda7f7b77053035c
MANIKANDAN E <89540495+ManikandanElavarasan@users.noreply.github.com>
2021-09-20 13:25:28 +0530

    Add files via upload

---
+++ LogicalCodesPractice/arm.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class arm {
+
+	public static void main(String[] args) {
+		//153-->1*1*1 +5*5*5 + 3*3*3=153
+		int n=370;
+		int temp=n;
+		int r,sum=0;
+		while(temp>0)
+		{
+			r=temp%10;
+			sum=sum+(r*r*r);
+			temp=temp/10;
+			
+		}
+		System.out.println("the sum is  "+sum);
+		if(sum==n)
+		{
+			System.out.println("the given number is armstrong number  "+sum +"  =  "+n);
+		}
+		else
+		{
+			System.out.println("the given number is not armstrong number..");
+		}
+		
+		
+		
+	}
+
+}
