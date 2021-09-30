722ee758fdb64cf735712b66c47971d455e960dd
MANIKANDAN E <89540495+ManikandanElavarasan@users.noreply.github.com>
2021-09-30 15:04:10 +0530

    Add files via upload

---
+++ LogicalCodesPractice/LCMofTwoNumbers.java
@@ -0,0 +1,26 @@
+package first;
+
+import java.util.Scanner;
+
+public class LCMofTwoNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any Two Numbers: ");
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+	     
+		int lcm=(num1>num2) ? num1:num2;
+		while(true)
+		{
+			if(lcm%num1==0 && lcm%num2==0)
+			{
+				System.out.println("The LCM of "+num1+" "+num2+" is"+lcm);
+				break;
+			}
+			lcm++;
+		}
+
+	}
+
+}
