d7c9ff3fb7da282876673a666a0b8ad06083b00a
MANIKANDAN E <89540495+ManikandanElavarasan@users.noreply.github.com>
2021-10-02 13:20:16 +0530

    Add files via upload

---
+++ LogicalCodesPractice/CalculatorInCommandLine.java
@@ -0,0 +1,21 @@
+package first;
+
+public class CalculatorInCommandLine {
+
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("The Addition of a and b is: "+(a+b));
+		System.out.println("The Subtraction of a and b is: "+(a-b));
+		System.out.println("The Multiplication of a and b is: "+(a*b));
+		System.out.println("The Division of a and b is: "+(a/b));
+
+		
+
+
+		
+		
+		
+	}
+
+}
