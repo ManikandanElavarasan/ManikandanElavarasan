aec8ea13eb24583efe069b0699c4a93eee16ae62
MANIKANDAN E <89540495+ManikandanElavarasan@users.noreply.github.com>
2021-10-03 10:47:05 +0530

    Add files via upload

---
+++ Programs/MinandMaxElementInAnArray.java
@@ -0,0 +1,39 @@
+package matrixarray;
+
+import java.util.Scanner;
+
+public class MinandMaxElementInAnArray {
+
+	public static void main(String[] args) {
+		int min,max;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Array size:");
+		int size=sc.nextInt();
+		int array[]=new int[size];
+		System.out.println("Enter the elements:");
+		for(int i=0;i<size;i++)
+		{
+		    array[i]=sc.nextInt();	
+		}
+		    min=max=array[0];
+		    for(int j=1;j<size;j++)
+				{
+					if(array[j]<min)
+					{
+						min=array[j];
+					}
+					else
+					{
+						max=array[j];
+					}
+				}
+			
+			System.out.println("Maximum element is: "+max);
+			System.out.println("Minimum element is: "+min);
+		
+		
+
+
+	}
+
+}
