bf59a804642fcb735ef60f61131b198318284be9
MANIKANDAN E <89540495+ManikandanElavarasan@users.noreply.github.com>
2021-10-03 10:42:09 +0530

    Add files via upload

---
+++ Programs/FindLeapYear.java
@@ -0,0 +1,36 @@
+package first;
+
+import java.util.Scanner;
+
+public class FindLeapYear {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a Year: ");
+		Scanner sc=new Scanner(System.in);
+		int year=sc.nextInt();
+	        if(year%4==0)  //Leap year means that should be divisible by 4
+	        {
+	        	if(year%100==0)//some century year may not be a leap year but those  will divisible by 4
+	        	{
+	        		if(year%400==0) //if a century is divisible by 400 then only we can say it as leap year
+	        		{
+	        			System.out.println(year+" is a Leap year ");
+	        		}
+	        		else
+	        		{
+	        			System.out.println(year+" is not a leap Year");
+	        		}
+	        	}
+	        	else
+	        	{
+	        		System.out.println(year+" is  a Leap year ");
+	        	}
+	        	
+	        }
+	        else
+	        {
+	        	System.out.println(year+" is Not a Leap year ");
+	        }
+	}}
+
+
